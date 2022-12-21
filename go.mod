d7c6155af1cc86022eb2bef82c7e739c342e5e15
mikumaycry <me@wbuntu.com>
2022-12-21 20:17:57 +0800

    on connect dial custom hosts

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/mikumaycry/mitm
 
 go 1.19
 
-require github.com/AdguardTeam/gomitmproxy v0.2.1
+require (
+	github.com/AdguardTeam/golibs v0.4.0
+	github.com/AdguardTeam/gomitmproxy v0.2.1
+)
 
 require (
-	github.com/AdguardTeam/golibs v0.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
-	"log"
 	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
+
+	"github.com/AdguardTeam/golibs/log"
 
 	"github.com/AdguardTeam/gomitmproxy"
 	"github.com/AdguardTeam/gomitmproxy/mitm"
@@ -26,7 +29,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	mitmConfig, err := mitm.NewConfig(x509c, privateKey, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -49,4 +51,81 @@ func main() {
 
 	// Clean up
 	proxy.Close()
+}
+
+func OnConnect() func(session *gomitmproxy.Session, proto, addr string) net.Conn {
+	file, err := os.Open("/opt/homebrew/etc/mitm/hosts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	hostMap := map[string]string{}
+	wildcardMap := map[string]string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		str := scanner.Text()
+		if strings.HasPrefix(str, "#") || len(str) == 0 {
+			continue
+		}
+		items := strings.Split(str, " ")
+		if len(items) != 2 {
+			log.Error("invalid record: %s", str)
+			continue
+		}
+		ip := items[0]
+		domain := items[1]
+		fileds := strings.Split(domain, ".")
+		if len(fileds) < 2 {
+			log.Error("invalid domain: %s", domain)
+			continue
+		}
+		if fileds[0] == "*" {
+			suffix := strings.Join(fileds[1:], ".")
+			wildcardMap[suffix] = ip
+			log.Info("add suffix domain: %s -> %s", domain, ip)
+		} else {
+			hostMap[domain] = ip
+			log.Info("add full domain: %s -> %s", domain, ip)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	const dialTimeout = 15 * time.Second
+	dialer := &net.Dialer{
+		Timeout:   dialTimeout,
+		KeepAlive: dialTimeout,
+	}
+	return func(session *gomitmproxy.Session, proto, addr string) net.Conn {
+		host, port, err := net.SplitHostPort(addr)
+		if err != nil {
+			log.Error("invalid addr: %s", addr)
+			return nil
+		}
+		if net.ParseIP(host) != nil {
+			return nil
+		}
+		var dstHost string
+		if v, ok := hostMap[host]; ok {
+			dstHost = v
+		} else {
+			for k, v := range wildcardMap {
+				if strings.HasSuffix(host, k) {
+					dstHost = v
+					break
+				}
+			}
+		}
+		if len(dstHost) == 0 {
+			return nil
+		}
+		dstAddr := net.JoinHostPort(dstHost, port)
+		log.Println(addr, " >>>> ", dstAddr)
+		dst, err := dialer.Dial(proto, dstAddr)
+		if err != nil {
+			log.Error("dial custom addr: %s", err)
+			return nil
+		}
+		return dst
+	}
 }
